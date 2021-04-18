d681e19d4ca4c743be10ec138aaee200e861055d
walpurgisnatch <Kawemirs@gmail.com>
2021-04-18 18:49:46 +0700

    fixt

--- src/stepster.lisp
+++ src/stepster.lisp
@@ -1,8 +1,6 @@
 (defpackage stepster
-  (:use :cl
-        :stepster.parser)
-  (:export :download-all-images))
+  (:use :cl))
 
 (in-package :stepster)
 
-;(cl-reexport:reexport-from :stepster.parser)
+(cl-reexport:reexport-from :stepster.parser)
