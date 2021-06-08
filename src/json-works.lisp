7f08920ac9365b4966a4d21a04855798fb06aadf
walpurgisnatch <kawemirs@gmail.com>
2021-06-09 09:29:16 +1000

    json-works

--- src/json-works.lisp
+++ src/json-works.lisp
@@ -2,9 +2,28 @@
 (defpackage stepster.json-works
   (:use :cl)
   (:export
-   :getj))
+   :getj
+   :findj
+   :intern-k))
 
 (in-package :stepster.json-works)
 
 (defun getj (list key)
-    (getf list (intern (string-downcase (string key)) "KEYWORD")))
+    (getf list (intern-k key)))
+
+(defun intern-k (item)
+    (intern (string-downcase (string item)) "KEYWORD"))
+
+(defun findj (list k &optional (acc nil))
+    (let ((key (intern-k k)))
+        (cond ((null list) nil)
+              ((equal (car list) key)
+               (cons (reverse (cons (car list) acc))
+                     (cadr list)))
+              ((consp (cadr list))
+               (or (findj (cadr list) k (cons (car list) acc))
+                   (findj (cddr list) k acc)))
+              ((consp (car list))
+               (or (findj (car list) k acc)
+                   (findj (cddr list) k acc)))
+              (t (findj (cdr list) k acc)))))
