c5e76346ef23cc4fb46d90e05b032e5453fb0782
walpurgisnatch <kawemirs@gmail.com>
2021-06-11 10:07:56 +1000

    intern list

--- src/json-works.lisp
+++ src/json-works.lisp
@@ -13,13 +13,25 @@
     (car (last x))) 
 
 (defun getj (list key)
-    (getf list (intern-k key)))
+    (getf list (internk key)))
 
-(defun intern-k (item)
+(defun internk (item)
     (intern (string-downcase (string item)) "KEYWORD"))
 
-(defun intern-ks (item)
-    (mapcar #'intern-k item))
+(defun intern2 (item)
+    (intern (string-downcase (string item))))
+
+(defun internks (item)
+    (mapcar #'internk item))
+
+(defun intern-list (list)
+    (loop for item in list
+          for i from 1
+          if (consp item)
+            collect (mapcar #'intern2 item)
+          else if (oddp i)
+                 collect (internk item)
+          else collect (intern2 item)))
 
 (defun flatten (x)
     (labels ((rec (x acc)
@@ -37,17 +49,16 @@
           (t (and (member-list (car x) (car y))
                   (member-list (cdr x) (cdr y))))))
 
-
 (defun findj (list keys)
     (flet ((collect2 (list keys)
                (mapcar #'(lambda (item) (getf list item)) keys)))
         (flatten (loop for (x y) in keys
               with result do
-                (setf result (jfinder list (intern-ks x)))
+                (setf result (jfinder list (internks x)))
               if (consp (car result))
                 collect (loop for item in result
-                              collect (collect2 item (intern-ks y)))
-              else collect (collect2 result (intern-ks y))))))
+                              collect (collect2 item (internks y)))
+              else collect (collect2 result (internks y))))))
         
 
 (defun jfinder (list key &optional (acc nil))
@@ -62,3 +73,5 @@
            (or (jfinder (cadr list) key (cons (car list) acc))
                (jfinder (cddr list) key acc)))
           (t (jfinder (cdr list) key acc))))
+
+        
