d8b00fc8dfe67aba68f46921f9e559ed56287fff
walpurgisnatch <kawemirs@gmail.com>
2021-06-24 14:08:24 +1000

    json-parsing v2

--- src/json-works.lisp
+++ src/json-works.lisp
@@ -1,6 +1,13 @@
 (in-package :cl-user)
 (defpackage stepster.json-works
   (:use :cl)
+  (:import-from :stepster.utils
+                :reverse-group
+                :getj
+                :internks
+                :carlast
+   :intern-list
+   :member-list)
   (:export
    :getj
    :findj
@@ -9,87 +16,26 @@
    :intern-list
    :get-values
    :pack-with
-   :pack-to-json))
+   :pack-to-json
+   :collect-json))
 
 (in-package :stepster.json-works)
 
-(defun carlast (x)
-    (car (last x))) 
+(defun pack-to-json (keys values)
+    (jonathan:to-json (intern-list (pack-with keys values))))
 
-(defun getj (list key)
-    (getf list (internk key)))
+(defun pack-with (keys values)
+    (loop for item in values collect
+      (loop for i in keys
+            for j in item
+            collect i
+            collect j)))
 
-(defun internk (item)
-    (cond ((numberp item)
-           item)
-          ((stringp item)
-           (intern (format nil "~a" item) "KEYWORD"))
-        (t (intern (string-downcase (string item)) "KEYWORD"))))
-
-(defun internks (item)
-    (if (consp item)
-        (mapcar #'internk item)
-        (internk item)))
-
-(defun intern2 (item)
-    (if (numberp item)
-        item
-        (intern (string-downcase (string item)))))
-    
-(defun flatten (x)
-    (labels ((rec (x acc)
-                 (cond ((null x) acc)
-                       ((atom x) (cons x acc))
-                       (t (rec (car x) (rec (cdr x) acc))))))
-        (rec x nil)))
-
-(defun intern-list (list)
-    (loop for item in list
-          for i from 1
-          if (consp item)
-            collect item
-          else if (oddp i)
-                 collect (internk item)
-          else collect (intern2 item)))
-
-(defun pack-to-json (x y)
-    (jonathan:to-json (intern-list (pack-with x y))))
-
-(defun pack-with (x y)
-    (loop for i in x
-          for j in y
-          collect i
-          collect j))
-
-(defun member-list (x y)
-    (cond ((null x) t)
-          ((atom x)
-           (equal x (if (atom y) y (car y))))
-          ((atom y)
-           (equal x y))
-          (t (and (member-list (car x) (car y))
-                  (member-list (cdr x) (cdr y))))))
-(defun get-values (list keys &optional test)
+(defun get-values (list keys)
     (if (consp (car list))
-        (loop with tmp
-              with tmp1
-              for i in list
-              when (block continue
-                       (setf tmp (mapcar #'(lambda (item) (setf tmp1 (getf i item))
-                                               (if (and test
-                                                        (funcall test tmp1))
-                                                   (return-from continue)
-                                                   tmp1))
-                                         (internks keys))))
-                collect tmp)
+        (loop for i in list
+              collect (mapcar #'(lambda (item) (getf i item)) (internks keys)))
         (mapcar #'(lambda (item) (getf list item)) (internks keys))))
-
-(defun findj (json list keys)
-    (let ((result (jfinder json (internks list))))
-        (if (consp (car result))
-            (loop for item in result
-                  collect (get-values item keys))
-            (get-values result keys))))
 
 (defun jfinder (list key &optional (acc nil))
     (cond ((null list) nil)
@@ -104,4 +50,15 @@
                (jfinder (cddr list) key acc)))
           (t (jfinder (cdr list) key acc))))
 
-        
+(defun fj (json keys &optional acc list)    
+    (cond ((null json) acc)
+          ((equal (car keys) (car json))
+           (fj list (cdr keys) (cons (cadr json) acc) list))
+          ((every #'consp json)
+           (fj (car json) keys (fj (cdr json) keys acc list) (car json)))
+          ((consp (cadr json))
+           (fj (cadr json) keys (fj (cddr json) keys acc list) list))
+          (t (fj (cddr json) keys acc list))))
+
+(defun collect-json (json keys)
+    (reverse-group (fj json (internks keys) nil json) (length keys)))
